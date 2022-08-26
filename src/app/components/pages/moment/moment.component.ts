6043068037b7b2cc51b29853a21b4f3a6a617485
logm <luiz-augusto.gomes@gft.com>
2022-08-26 17:18:56 -0300

    comitando atualizacoes

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormGroupDirective, } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { MomentService } from 'src/app/services/moment.service';
-import { MessagesService } from 'src/app/services/messages.service';
-import { CommentService } from 'src/app/services/comment.service';
 import { Moment } from 'src/app/Moment';
-import { Comment } from 'src/app/Comment';
+import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Comment } from 'src/app/Comment';
+import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
+import { CommentService } from 'src/app/services/comment.service';
 @Component({
   selector: 'app-moment',
   templateUrl: './moment.component.html',
   styleUrls: ['./moment.component.css']
 })
 export class MomentComponent implements OnInit {
+
   moment?: Moment;
   baseApiUrl = environment.baseApiUrl;
-  faEdit = faEdit;
-  faTrash = faTrash;
 
-  comentForm!: FormGroup;
+  faTimes = faTimes;
+  faEdit = faEdit;
+
+  commentForm!: FormGroup;
 
   constructor(
     private momentService: MomentService,
@@ -28,53 +30,49 @@ export class MomentComponent implements OnInit {
     private messagesService: MessagesService,
     private router: Router,
     private commentService: CommentService,
+
     ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.momentService
+    .getMoment(id)
+    .subscribe((item) => (this.moment = item.data))
 
-    this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data));
-
-    this.comentForm = new FormGroup({
+    this.commentForm = new FormGroup({
       text: new FormControl('', [Validators.required]),
       username: new FormControl('', [Validators.required]),
-    });
+    })
   }
 
-  get text() {
-    return this.comentForm.get('text')!;
+  get text(){
+    return this.commentForm.get('text')!;
   }
 
-  get username() {
-    return this.comentForm.get('username')!;
+  get username(){
+    return this.commentForm.get('username')!;
   }
 
-  async removeHandler(id: number) {
-    await this.momentService.removeMoment(id).subscribe();
-
-    this.messagesService.add("Momento excluído com sucesso!");
-
-    this.router.navigate(['/'])
+  async removeHandler(id:number) {
+    await this.momentService.removeMoment(id).subscribe()
+    this.messagesService.add("Momento excluído com sucesso")
+    this.router.navigate(['/']);
   }
 
-  async onSubmit(formDirective: FormGroupDirective) {
-    if (this.comentForm.invalid) {
-      return;
+  async onSubmit(formDirective: FormGroupDirective){
+    if(this.commentForm.invalid){
+      return
     }
 
-    const data: Comment = this.comentForm.value;
-
-    data.momentId = Number(this.moment!.id);
-
+    const data: Comment = this.commentForm.value
+    data.momentId = Number(this.moment!.id)
     await this.commentService
     .createComment(data)
-    .subscribe((comment) => this.moment!.comments!.push(comment.data));
+    .subscribe((coment) => this.moment!.comments!
+    .push(coment.data));
 
-    this.messagesService.add("Comentário adicionado");
-
-    //reset form
-    this.comentForm.reset();
-
+    this.messagesService.add('Comentário adicionado!');
+    this.commentForm.reset();
     formDirective.resetForm();
   }
 
